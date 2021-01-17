e8013f82edce7b82ed06ae24d84f66d02e38fe06
Mattisonfire511 <74126804+Mattisonfire511@users.noreply.github.com>
2021-01-16 19:16:17 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@ input.onButtonPressed(Button.B, function () {
     player.change(LedSpriteProperty.X, 1)
     music.playTone(262, music.beat(BeatFraction.Quarter))
 })
+let bullet_4: game.LedSprite = null
 let bullet_3: game.LedSprite = null
 let player: game.LedSprite = null
 let _5 = 0
@@ -35,6 +36,10 @@ basic.forever(function () {
 basic.forever(function () {
     basic.pause(125)
     bullet_speed += -2
+    if (bullet_speed < 750) {
+        basic.pause(125)
+        bullet_speed += 1
+    }
 })
 basic.forever(function () {
     if (!(game.isGameOver())) {
@@ -50,17 +55,24 @@ basic.forever(function () {
 basic.forever(function () {
     if (player.isTouching(bullet_1)) {
         game.removeLife(1)
-        basic.pause(bullet_speed + 100)
+        basic.pause(bullet_speed)
     }
     if (player.isTouching(bullet_2)) {
         game.removeLife(1)
-        basic.pause(bullet_speed + 100)
+        basic.pause(bullet_speed)
     }
     if (game.score() > 4) {
         basic.pause(500)
         if (player.isTouching(bullet_3)) {
             game.removeLife(1)
-            basic.pause(bullet_speed + 100)
+            basic.pause(bullet_speed)
+        }
+    }
+    if (game.score() > 8) {
+        basic.pause(500)
+        if (player.isTouching(bullet_4)) {
+            game.removeLife(1)
+            basic.pause(bullet_speed)
         }
     }
 })
@@ -92,6 +104,39 @@ basic.forever(function () {
             }
             bullet_3.change(LedSpriteProperty.Y, -4)
             bullet_3.set(LedSpriteProperty.X, randint(_4, _5))
+        }
+    }
+})
+basic.forever(function () {
+    if (game.score() > 8) {
+        _0 = 0
+        _1 = 4
+        _2 = 0
+        _3 = 4
+        _4 = 0
+        _5 = 4
+        bullet_1.delete()
+        bullet_2.delete()
+        bullet_3.delete()
+        bullet_1 = game.createSprite(randint(0, 4), 0)
+        bullet_2 = game.createSprite(randint(0, 4), 0)
+        bullet_3 = game.createSprite(randint(0, 4), 0)
+        bullet_4 = game.createSprite(randint(0, 4), 0)
+        for (let index = 0; index < 10000000000; index++) {
+            basic.pause(5000)
+        }
+    }
+})
+basic.forever(function () {
+    if (game.score() > 8) {
+        if (!(game.isGameOver())) {
+            for (let index = 0; index < 5; index++) {
+                music.playTone(131, music.beat(BeatFraction.Quarter))
+                basic.pause(bullet_speed)
+                bullet_4.change(LedSpriteProperty.Y, 1)
+            }
+            bullet_4.change(LedSpriteProperty.Y, -4)
+            bullet_4.set(LedSpriteProperty.X, randint(_4, _5))
         }
     }
 })
