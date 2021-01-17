035736958b9b896a7c0f0f935ac4496807324904
Mattisonfire511 <74126804+Mattisonfire511@users.noreply.github.com>
2021-01-16 18:19:27 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,97 @@
+input.onButtonPressed(Button.A, function () {
+    player.change(LedSpriteProperty.X, -1)
+    music.playTone(247, music.beat(BeatFraction.Quarter))
+})
+input.onButtonPressed(Button.B, function () {
+    player.change(LedSpriteProperty.X, 1)
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+})
+let bullet_3: game.LedSprite = null
+let player: game.LedSprite = null
+let _5 = 0
+let _4 = 0
+let _0 = 0
+let _1 = 2
+let _2 = 3
+let _3 = 5
+let bullet_speed = 1500
+game.setLife(3)
+let bullet_1 = game.createSprite(randint(0, 1), 0)
+basic.pause(50)
+let bullet_2 = game.createSprite(randint(2, 4), 0)
+player = game.createSprite(2, 3)
+basic.forever(function () {
+    if (!(game.isGameOver())) {
+        for (let index = 0; index < 5; index++) {
+            music.playTone(131, music.beat(BeatFraction.Quarter))
+            basic.pause(bullet_speed)
+            bullet_1.change(LedSpriteProperty.Y, 1)
+        }
+        bullet_1.change(LedSpriteProperty.Y, -4)
+        bullet_1.set(LedSpriteProperty.X, randint(_0, _1))
+        game.addScore(1)
+    }
+})
+basic.forever(function () {
+    basic.pause(125)
+    bullet_speed += -2
+})
+basic.forever(function () {
+    if (!(game.isGameOver())) {
+        for (let index = 0; index < 5; index++) {
+            music.playTone(131, music.beat(BeatFraction.Quarter))
+            basic.pause(bullet_speed)
+            bullet_2.change(LedSpriteProperty.Y, 1)
+        }
+        bullet_2.change(LedSpriteProperty.Y, -4)
+        bullet_2.set(LedSpriteProperty.X, randint(_2, _3))
+    }
+})
+basic.forever(function () {
+    if (player.isTouching(bullet_1)) {
+        game.removeLife(1)
+        basic.pause(bullet_speed + 100)
+    }
+    if (player.isTouching(bullet_2)) {
+        game.removeLife(1)
+        basic.pause(bullet_speed + 100)
+    }
+    if (game.score() > 4) {
+        basic.pause(500)
+        if (player.isTouching(bullet_3)) {
+            game.removeLife(1)
+            basic.pause(bullet_speed + 100)
+        }
+    }
+})
+basic.forever(function () {
+    if (game.score() > 4) {
+        _0 = 0
+        _1 = 1
+        _2 = 2
+        _3 = 2
+        _4 = 3
+        _5 = 4
+        bullet_1.delete()
+        bullet_2.delete()
+        bullet_1 = game.createSprite(randint(0, 1), 0)
+        bullet_2 = game.createSprite(randint(2, 2), 0)
+        bullet_3 = game.createSprite(randint(3, 4), 0)
+        for (let index = 0; index < 10000000000; index++) {
+            basic.pause(5000)
+        }
+    }
+})
+basic.forever(function () {
+    if (game.score() > 4) {
+        if (!(game.isGameOver())) {
+            for (let index = 0; index < 5; index++) {
+                music.playTone(131, music.beat(BeatFraction.Quarter))
+                basic.pause(bullet_speed)
+                bullet_3.change(LedSpriteProperty.Y, 1)
+            }
+            bullet_3.change(LedSpriteProperty.Y, -4)
+            bullet_3.set(LedSpriteProperty.X, randint(_4, _5))
+        }
+    }
+})
